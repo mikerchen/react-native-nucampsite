9cd21278973d5710bca6cfe10bcaf9f406faa59f
Sylvia Seo <sylviaseo@sylvias-air.lan>
2021-11-16 23:05:37 -0800

    coverted all components to redux

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
 import { ScrollView, Text, FlatList } from 'react-native';
 import { ListItem, Card } from 'react-native-elements';
-import { PARTNERS } from '../shared/partners';
+import { connect } from 'react-redux';
+import { baseUrl } from '../shared/baseUrl';
+
+const mapStateToProps = state => {
+    return {
+        partners: state.partners
+    };
+};
+
+function Mission() {
+    return(
+        <Card>
+            <Text style={{margin: 10}}>
+            We present a curated database of the best campsites in the vast woods and backcountry of the World Wide Web Wilderness. We increase access to adventure for the public while promoting safe and respectful use of resources. The expert wilderness trekkers on our staff personally verify each campsite to make sure that they are up to our standards. We also present a platform for campers to share reviews on campsites they have visited with each other.
+            </Text>
+        </Card>
+    )
+}
 
 class About extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            partners: PARTNERS
-        }
-    }
 
     static navigationOptions = {
         title:'About Us'
@@ -22,7 +33,7 @@ class About extends Component {
                 <ListItem
                     title={item.name}
                     subtitle={item.description}
-                    leftAvatar={{ source: require('./images/bootstrap-logo.png')}}
+                    leftAvatar={{ source: {uri: baseUrl + item.image}}}
                 />
             );
         };
@@ -32,7 +43,7 @@ class About extends Component {
                 <Mission/>                
                 <Card title="Community Partners">
                     <FlatList
-                        data={this.state.partners}
+                        data={this.props.partners.partners}
                         renderItem={renderPartner}
                         keyExtractor={item => item.id.toString()}
                     >
@@ -43,14 +54,4 @@ class About extends Component {
     };
 }
 
-function Mission() {
-    return(
-        <Card>
-            <Text style={{margin: 10}}>
-            We present a curated database of the best campsites in the vast woods and backcountry of the World Wide Web Wilderness. We increase access to adventure for the public while promoting safe and respectful use of resources. The expert wilderness trekkers on our staff personally verify each campsite to make sure that they are up to our standards. We also present a platform for campers to share reviews on campsites they have visited with each other.
-            </Text>
-        </Card>
-    )
-}
-
-export default About;
+export default connect(mapStateToProps)(About);
