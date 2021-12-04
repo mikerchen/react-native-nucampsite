bfd15de3fa37e133828da12b40f48096d74d60fe
Sylvia Seo <sylviaseo@sylvias-air.lan>
2021-12-03 23:01:14 -0800

    week 4 weekly assignment

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -4,6 +4,7 @@ Picker, Switch, Button, Alert } from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import * as Animatable from 'react-native-animatable';
+import * as Notifications from 'expo-notifications';
 
 class Reservation extends Component {
     constructor(props) {
@@ -14,7 +15,6 @@ class Reservation extends Component {
             hikeIn: false,
             date: new Date(),
             showCalendar: false,
-            showModal: false
         };
     }
 
@@ -22,13 +22,34 @@ class Reservation extends Component {
         title: 'Reserve Campsite'
     }
 
-    toggleModal() {
-        this.setState({showModal: !this.state.showModal})
-    }
-
     handleReservation() {
         console.log(JSON.stringify(this.state));
-        this.toggleModal();
+        const message = `Number of Campers: ${this.state.campers}
+                        \nHike-In? ${this.state.hikeIn}
+                        \nDate: ${this.state.date.toLocaleDateString('en-US')}`;
+        Alert.alert(
+            'Begin Search?',
+            "Number of Campers: " + this.state.campers + '\n\n' + "Hike-In? " + this.state.hikeIn + '\n\n' + "Date: " +this.state.date,
+            [
+                {
+                    text: 'Cancel',
+                    onPress: () => {
+                        this.resetForm();
+                        console.log('Alert Cancelled');
+                    },
+                    style: 'cancel'
+                },
+                {
+                    text: 'OK',
+                    onPress: () => {
+                        this.presentLocalNotification(this.state.date.toLocaleDateString('en-US'));
+                        this.resetForm();
+                        console.log('Alert OKed');
+                    },
+                }
+            ],
+            { cancelable: false }
+        )
     }
 
     resetForm() {
@@ -37,8 +58,32 @@ class Reservation extends Component {
             hikeIn: false,
             date: new Date(),
             showCalendar: false,
-            showModal: false
         });
+    }
+
+    async presentLocalNotification(date) {
+        function sendNotification() {
+            Notifications.setNotificationHandler({
+                handleNotification: async() => ({
+                    shouldShowAlert: true
+                })
+            });
+
+            Notifications.scheduleNotificationAsync({
+                content: {
+                    title: 'Your Campsite Reservation Search',
+                    body: `Search for ${date} requested`
+                },
+                trigger: null
+            })
+        }
+        let permissions = await Notifications.getPermissionsAsync();
+        if(!permissions.granted) {
+            permissions = await Notifications.requestPermissionsAsync();
+        }
+        if(permissions.granted) {
+            sendNotification();
+        }
     }
 
     render() {
@@ -98,30 +143,7 @@ class Reservation extends Component {
                 )}
                 <View style={styles.formRow}>
                     <Button 
-                        onPress={() => 
-                            Alert.alert(
-                                'Begin Search?',
-                                "Number of Campers: " + this.state.campers + '\n\n' + "Hike-In? " + this.state.hikeIn + '\n\n' + "Date: " +this.state.date,
-                                [
-                                    {
-                                        text: 'Cancel',
-                                        onPress: () => {
-                                            this.resetForm();
-                                            console.log('Alert Cancelled');
-                                        },
-                                        style: 'cancel'
-                                    },
-                                    {
-                                        text: 'OK',
-                                        onPress: () => {
-                                            this.resetForm();
-                                            console.log('Alert OKed');
-                                        },
-                                    }
-                                ],
-                                { cancelable: false }
-                            )
-                        }
+                        onPress={() => this.handleReservation()}
                         title='Search'
                         color='#5647DD'
                         accessibilityLabel='Tap me to search for available campsites to reserve'
